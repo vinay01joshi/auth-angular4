1cb8a37568d52d3a3b3bbe233518f345cffd39e5
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-01 12:24:08 +0530

    Authentication and Autorization | Showing or Hiding Elements based on the User's Role

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -5,7 +5,7 @@ export function fakeBackendFactory(
     backend: MockBackend, 
     options: BaseRequestOptions) {
         
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6dHJ1ZX0.iy8az1ZDe-_hS8GLDKsQKgPHvWpHl0zkQBqy1QIPOkA';
+  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlZpbmF5IEpvc2hpIiwiYWRtaW4iOmZhbHNlfQ.-GhwP_5Nm0vSLthOKNqvqqZLHhqlpmR181GyNLW-nsQ';
     
   backend.connections.subscribe((connection: MockConnection) => {
     // We are using the setTimeout() function to simulate an 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -40,4 +40,12 @@ export class AuthService {
         // return !isExpired;
     }
 
+    get currentUser(){
+        let token = localStorage.getItem('token');
+
+        if(!token) return null;
+
+        return new JwtHelper().decodeToken(token);
+    }
+
 }
